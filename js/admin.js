d1bdad5453c0d18d023c42421f3936fd2ab89d01
alanChibilisco <alan.chibilisco@gmail.com>
2021-12-04 12:13:07 -0300

    se completo la tarea de mostrar los productos guardados en el local storage

--- js/admin.js
+++ js/admin.js
@@ -2,7 +2,7 @@ import {requiredField, validateNumbers, validateURL, generalValidation} from "./
 
 import{Product} from "./productClass.js";
 
-import{random} from "./randomCode.js";
+import{random, regCodeLocalStorage} from "./randomCode.js";
 //variables
 
 let inputCode=document.getElementById('code');
@@ -44,6 +44,11 @@ inputUrl.addEventListener('blur', ()=>{
 form.addEventListener('submit', saveProduct);
 
 
+//llamo a la carga inicial de los productos en la tabla
+
+initCharge();
+
+
 //funcion para guardar el producto
 function saveProduct(e){
     e.preventDefault();
@@ -58,5 +63,58 @@ function saveProduct(e){
 
 function createProduct(){
     let newProduct=new Product(random(), inputCategory.value, inputDescription.value, inputModel.value, inputBrand.value, inputPrice.value, inputUrl.value);
-    console.log(newProduct);
+    //console.log(newProduct);
+    regProducts.push(newProduct);
+    //console.log(regProducts);
+    cleanForm();
+    Swal.fire(
+        'Producto creado',
+        'Su producto fue correctamente cargado',
+        'success'
+      )
+
+      createRow(newProduct);
+}
+
+//funcion para limpiar el formulario
+function cleanForm(){
+    form.reset();
+    inputCategory.className='form-control';
+    inputDescription.className='form-control';
+    inputModel.className='form-control';
+    inputBrand.className='form-control';
+    inputPrice.className='form-control';
+    inputUrl.className='form-control';
+    saveRegProductsLocalStorage();
+    regCodeLocalStorage();
+}
+
+//funcion para guardar el arreglo de productos dentro del localStorage
+function saveRegProductsLocalStorage(){
+    localStorage.setItem('regProductsLocalStorage', JSON.stringify(regProducts));
+}
+
+//funcion para crear una nueva fila en la seccion 2
+function createRow(newProduct){
+    let productTable=document.getElementById('table');
+    productTable.innerHTML+=`<tr>
+    <th scope="row">${newProduct.code}</th>
+    <td>${newProduct.category}</td>
+    <td>${newProduct.description}</td>
+    <td>${newProduct.model}</td>
+    <td>${newProduct.brand}</td>
+    <td>${"$"+newProduct.price}</td>
+    <td>${newProduct.url}</td>
+    <td class="text-center"><button class="btn btn-warning" type="submit"><i class="far fa-edit"></i></button> <button class="btn btn-danger" type="submit"><i class="fas fa-trash"></i></button></td>
+  </tr>`
+}
+
+//funcion para la carga inicial de la tabla tomando los datos de productos desde el localStorage
+
+function initCharge(){
+    if(regProducts.length>0){
+        regProducts.forEach(element => {
+            createRow(element);
+        });
+    }
 }
