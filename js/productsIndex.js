28a0c99bdece656d21b64ff7b64c7cfa17d9a00d
SantiagoAdan <santiadan25@gmail.com>
2021-12-08 16:56:15 -0300

    se estilizo el index

--- js/productsIndex.js
+++ js/productsIndex.js
@@ -1,32 +1,25 @@
 let regProducts =
   JSON.parse(localStorage.getItem("regProductsLocalStorage")) || [];
 
-  let productsCards = document.getElementById("productsCards");
+let productsCards = document.getElementById("productsCards");
 
 let filter = document.getElementById("filter");
 
- regProducts.forEach(product => {
-
-     createCard(product);
-
- });
+regProducts.forEach((product) => {
+  createCard(product);
+});
 
 //funcion para crear las cards de los productos almacenados en localStorage
 function createCard(product) {
-  
-  
-  productsCards.innerHTML += `<div class="col-sm-12 col-md-6 col-lg-6 my-2">
-    <div class="card">
+  productsCards.innerHTML += `<div class="col-sm-12 col-md-6 col-lg-3 my-2" width=100%>
+    <div class="card h-100">
       <div class="card-body">
         <img src="${product.url}" alt="${"Imagen de " + product.description}" 
-        onerror="this.src='img/image-not-found.png';" width=100%>
-        <h5 class="card-title">${
-          product.description + "-Modelo-" + product.model
-        }</h5><h6 class="card-title">${
-    "Categoria: " + product.category
-  }</h6><span class="badge rounded-pill bg-dark">${
-    "$ " + product.price + ",00"
-  }</span></h5>
+        onerror="this.src='img/image-not-found.png';>
+        <h5 class="card-title">${product.description + "-Modelo-" + product.model
+    }</h5><h6 class="card-title">${"Categoria: " + product.category
+    }</h6><span class="badge rounded-pill bg-dark">${"$ " + product.price + ",00"
+    }</span></h5>
       </div>
     </div>
   </div>`;
@@ -38,43 +31,47 @@ filter.addEventListener("change", () => {
 //funcion para filtrar los productos
 function filterProd(value) {
   if (value === "Todo") {
-    productsCards.innerHTML='';
+    productsCards.innerHTML = "";
     regProducts.forEach((product) => {
       createCard(product);
     });
-  }else if(value==='A-Z'){
-    productsCards.innerHTML='';
-    let newRegProd=regProducts.sort(SortArray);
-    newRegProd.forEach((product)=>{
+  } else if (value === "A-Z") {
+    productsCards.innerHTML = "";
+    let newRegProd = regProducts.sort(SortArray);
+    newRegProd.forEach((product) => {
       createCard(product);
     });
-    
-    regProducts=JSON.parse(localStorage.getItem("regProductsLocalStorage")) || [];
-    
-  }else if(value==='Z-A'){
-    productsCards.innerHTML='';
-    let newRegProd=regProducts.sort(SortArrayZA);
-    newRegProd.forEach((product)=>{
+
+    regProducts =
+      JSON.parse(localStorage.getItem("regProductsLocalStorage")) || [];
+  } else if (value === "Z-A") {
+    productsCards.innerHTML = "";
+    let newRegProd = regProducts.sort(SortArrayZA);
+    newRegProd.forEach((product) => {
       createCard(product);
     });
-    
-    regProducts=JSON.parse(localStorage.getItem("regProductsLocalStorage")) || [];
-    
-  }else{
-    productsCards.innerHTML='';
+
+    regProducts =
+      JSON.parse(localStorage.getItem("regProductsLocalStorage")) || [];
+  } else {
+    productsCards.innerHTML = "";
     let newRegProd = regProducts.filter((product) => {
       return product.category === value;
     });
-    newRegProd.forEach((product)=>{
+    newRegProd.forEach((product) => {
       createCard(product);
     });
-  }  
+  }
 }
 //funcion para compara de A-Z segun la descripcion
-function SortArray(x, y){
- return x.description.localeCompare(y.description, 'en', { sensitivity: 'base' });
+function SortArray(x, y) {
+  return x.description.localeCompare(y.description, "en", {
+    sensitivity: "base",
+  });
 }
 //funcion para compara de Z-A segun la descripcion
- function SortArrayZA(x, y){
-   return y.description.localeCompare(x.description, 'en', { sensitivity: 'base' });
-  }
+function SortArrayZA(x, y) {
+  return y.description.localeCompare(x.description, "en", {
+    sensitivity: "base",
+  });
+}
