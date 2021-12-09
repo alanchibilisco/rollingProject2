ed6b58a34eb372659592b1a5df278610a795e482
alanChibilisco <alan.chibilisco@gmail.com>
2021-12-09 19:53:18 -0300

    se completo la logica para logear con un usuario distinto de admin

--- js/login.js
+++ js/login.js
@@ -1,14 +1,12 @@
 let superUser={name:'admin', password:'admin'};
-
-
+let regUser=JSON.parse(localStorage.getItem('regUser'));
+console.log(regUser);
 let inputUser=document.getElementById('inputUser');
 let inputPass=document.getElementById('inputPass');
 let formLogin=document.getElementById('formLogin');
 let sesionInit=false;
 
-//console.log(superUser);
-//console.log(inputUser.value);
-//console.log(inputPass.value);
+
 //asociando el evento submit
 formLogin.addEventListener('submit', login);
 
@@ -25,12 +23,41 @@ function login(e){
             window.location.replace("index.html");
 
         }else{
-            alert('Contraseña Incorrecta, ingrese nuevamente');
+            alert('Contraseña Incorrecta. Ingrese nuevamente');
             console.log('contraseña incorrecta');
         }
-    }else {}
+    }
+
+    if(inputUser.value===findUser(inputUser.value).email){
+        //console.log('desde '+findUser(inputUser.value).email);
+        if(inputPass.value===findUser(inputUser.value).pass){
+            //console.log('correctamente logeado');
+            let newUser=findUser(inputUser.value);
+            sesionInit=true;
+            localStorage.setItem('stateSesion', JSON.stringify(sesionInit));
+            localStorage.setItem('user', JSON.stringify(newUser));
+            //console.log('fin logeo usario: '+newUser.name);
+            window.location.replace("index.html");
+        }else{
+            alert('Contraseña Incorrecta. Ingrese nuevamente');
+        }
+    }else{
+        alert('Usuario Incorrecto. Ingrese nuevamente');
+    }
     
+  
 
     
 }
 
+function findUser(email){
+    let newUser=regUser.find((user)=>{
+        return (user.email===email)
+    });
+    if(newUser!==undefined){
+        return newUser;
+    }else{
+        return '';
+    }
+    
+}
